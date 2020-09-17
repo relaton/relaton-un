16407378566e8b5ae6ab5ef85ec023cd8c336cb1
Andrei Kislichenko <android.2net@gmail.com>
2020-09-18 00:27:50 +0200

    update README

--- lib/relaton_un/hit_collection.rb
+++ lib/relaton_un/hit_collection.rb
@@ -6,18 +6,23 @@ require "http-cookie"
 module RelatonUn
   # Page of hit collection.
   class HitCollection < RelatonBib::HitCollection
-    AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) "\
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+    AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) "\
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     DOMAIN = "https://documents.un.org"
-    BOUNDARY = "----WebKitFormBoundary6hkaBvITDck8dHCn"
+    BOUNDARY = "----WebKitFormBoundaryVarT9Z7AFUzw2lma"
 
     # @param text [String] reference to search
     def initialize(text)
       super
       @uri = URI.parse DOMAIN
       @jar = HTTP::CookieJar.new
-      @http = Net::HTTP.new @uri.host, @uri.port
+      @http = Net::HTTP.new @uri.host, @uri.port # , "localhost", "8000"
       @http.use_ssl = true
+      # @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      # @http.cert_store = OpenSSL::X509::Store.new
+      # ca_file = "/Users/andrej/Library/Preferences/httptoolkit/ca.pem"
+      # @http.cert_store.set_default_paths
+      # @http.cert_store.add_file ca_file
       @http.read_timeout = 120
       if (form_resp = get_page)
         doc = Nokogiri::HTML page_resp(form_resp, text).body
@@ -50,17 +55,19 @@ module RelatonUn
     # @return [Array<String>]
     def form_data(form, text) # rubocop:disable Metrics/CyclomaticComplexity
       fd = form.xpath(
-        "//input[@type!='radio']",
-        "//input[@type='radio'][@checked]",
-        "//select[@name!='view:_id1:_id2:cbLang']",
+        "//input[@type!='radio']|"\
+        "//input[@type='radio'][@checked]|"\
+        "//select[@name!='view:_id1:_id2:cbLang']|"\
         "//textarea"
       ).reduce([]) do |m, i|
         v = case i[:name]
             when "view:_id1:_id2:txtSymbol" then text
+            when "view:_id1:_id2:rgTrunc" then "R"
             when "view:_id1:_id2:cbType" then "FP"
             when "view:_id1:_id2:cbSort" then "R"
             when "$$xspsubmitid" then "view:_id1:_id2:_id130"
-            when "$$xspsubmitscroll" then "0|167"
+            when "$$xspsubmitscroll" then "0|102"
+            when "view:_id1" then "view:_id1"
             else i[:value]
             end
         m << %{--#{BOUNDARY}}
@@ -77,7 +84,7 @@ module RelatonUn
       form = Nokogiri::HTML form_resp.body
       req = Net::HTTP::Post.new form.at("//form")[:action]
       set_headers req
-      req["Content-Type"] = "multipart/form-data, boundary=#{BOUNDARY}"
+      req["Content-Type"] = "multipart/form-data; boundary=#{BOUNDARY}"
       req.body = form_data(form, text).join("\r\n")
       resp = @http.request req
       get_page URI.parse(resp["location"]).request_uri
@@ -163,15 +170,18 @@ module RelatonUn
     # rubocop:disable Metrics/MethodLength
 
     # @param req [Net::HTTP::Get, Net::HTTP::Post]
-    def set_headers(req)
+    def set_headers(req) # rubocop:disable Metrics/AbcSize
       set_cookie req
       req["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,"\
-      "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      "image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;"\
+      "v=b3;q=0.9"
       req["Accept-Encoding"] = "gzip, deflate, br"
+      req["Accept-Language"] = "en-US;q=0.8,en;q=0.7"
       req["Cache-Control"] = "max-age=0"
       req["Connection"] = "keep-alive"
       req["Origin"] = "https://documents.un.org"
       req["Referer"] = "https://documents.un.org/prod/ods.nsf/home.xsp"
+      req["Sec-Fetch-Dest"] = "document"
       req["Sec-Fetch-Mode"] = "navigate"
       req["Sec-Fetch-Site"] = "same-origin"
       req["Sec-Fetch-User"] = "?1"
